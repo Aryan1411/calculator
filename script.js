e76d973de24c248eb20bb0baff31f479b64cb50a
Aryan Mishra <132837146+Aryan1411@users.noreply.github.com>
2025-01-30 15:19:15 +0530

    partial

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function calc(){
-    
+function log(msg){
+    let ul=document.getElementById('log_content');
+    let li=document.createElement('li');
+    let text=document.createTextNode(msg);
+    li.appendChild(text);
+    ul.appendChild(li);
+}
+function calculate(){
+    let expression=document.getElementById('value').value;
+    if(expression){
+        try{
+            let ans=eval(expression);
+            document.getElementById('value').value=ans;
+            log(expression+'='+ans);
+        }catch(e){
+            console.log(e)
+        }
+    }
 }
